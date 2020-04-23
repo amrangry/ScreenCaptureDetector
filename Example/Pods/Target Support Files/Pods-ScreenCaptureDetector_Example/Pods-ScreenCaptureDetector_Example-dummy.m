06dc4866655204998279bf28c071968c46bc43c6
AmrAngry <amr.elghadban@gmail.com>
2020-04-23 23:31:40 +0400

    initial commit for cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-ScreenCaptureDetector_Example/Pods-ScreenCaptureDetector_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScreenCaptureDetector_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ScreenCaptureDetector_Example
+@end
---
+++ Example/Pods/Target Support Files/ScreenCaptureDetector/ScreenCaptureDetector-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ScreenCaptureDetector : NSObject
+@end
+@implementation PodsDummy_ScreenCaptureDetector
+@end
