83955361bf65cc4990da02264e949c33430682f8
AmrAngry <amr.elghadban@gmail.com>
2020-04-27 00:26:59 +0400

    add comments

--- Source/NotificationName+Extension .swift
+++ Source/NotificationName+Extension .swift
@@ -1,6 +1,6 @@
 //
 //  NotificationName+Extension .swift
-//  MotoShop
+//  ADKATech.com
 //
 //  Created by AmrAngry on 4/25/20.
 //  Copyright © 2020 ADKA Tech. All rights reserved.
@@ -9,8 +9,8 @@
 import Foundation
 
 public extension Notification.Name {
-    /// notificatio.naem screenCapturingStarted
+    /// This notification is posted after the user start recording screen or mirror application screen
     static let screenCapturingStarted = Notification.Name(rawValue: "screenCapturingStarted")
-    /// notificatio.naem screenCapturingEnded
+    /// This notification is posted after the user end recording screen or mirror application screen
     static let screenCapturingEnded = Notification.Name(rawValue: "screenCapturingEnded")
 }
